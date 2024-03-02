f2993f1fabd7f63fde12541792ed6d7853ab94e0
borisbajzek <161962870+borisbajzek@users.noreply.github.com>
2024-03-02 17:51:27 +0000

    zadatak

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ name = input('What is your name?')
 print('heeeelllooooooo' + name)
 surname = 'Bajzek'
 print(10)
+
+n = input("abcdef")
+
+a = int(n[0])
+b = int(n[1])
+c = int(n[2])
+d = int(n[3])
+e = int(n[4])
+f = int(n[5])
+lijevo = a * c + 2 + f
+desno = b + d*e
+lijevo == desno
